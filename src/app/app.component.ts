a40ba14e1f3f3639ea166980dd05566096c8a9bd
omar2004712 <o.bahutair@gmail.com>
2023-07-22 15:25:25 +0400

    feat: implemeted the logic for the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = faker.lorem.sentence();
+  enteredText = '';
+
+  onInput(event: Event) {
+    const target = event.target as HTMLInputElement;
+
+    this.enteredText = target.value;
+  }
+
+  getClass(letter: string, i: number) {
+    if (i >= this.enteredText.length) {
+      return 'text-gray-400';
+    }
+
+    if (letter === this.enteredText[i]) {
+      return 'text-green-500';
+    }
+
+    return 'text-red-500 font-semibold';
+  }
 }
